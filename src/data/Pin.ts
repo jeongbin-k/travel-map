54c9f5c556d1e4d544e95c8043b983265be3e990
Jeongbin Kang <kangbin8109@gmail.com>
2026-04-23 15:05:04 +0900

    refactor: 한 지역당 여러 프로젝트를 등록할 수 있또록 PinData 스키마 개선

--- src/data/Pin.ts
+++ src/data/Pin.ts
@@ -1,46 +1,66 @@
 export interface PinData {
   id: number;
-  title: string;
+  projects: ProjectItem[];
   country: string;
   location1: string;
   location2: string;
   coords: [number, number];
-  description: string;
-  date: string;
-  tags: string[];
-  type: "Project" | "study";
-  link?: string;
   cardOffset: { x: number; y: number };
+}
+
+export interface ProjectItem {
+  title: string;
+  date: string;
+  type: "project" | "study";
+  description: string;
+  tags: string[];
+  link?: string;
 }
 
 export const pins: PinData[] = [
   {
     id: 1,
-    title: "Clippi",
+    projects: [
+      {
+        title: "Clippi",
+        date: "2026.03",
+        type: "project",
+        description: "북마크 관리 및 공유를 위한 풀스택 서비스",
+        tags: ["React", "TypeScript", "supabase"],
+        link: "https://clippi-seven.vercel.app/",
+      },
+      {
+        title: "intflow",
+        date: "2026.02",
+        type: "project",
+        description: "북마크 관리 및 공유를 위한 풀스택 서비스",
+        tags: ["React", "TypeScript", "supabase"],
+        link: "https://clippi-seven.vercel.app/",
+      },
+    ],
     country: "south korea",
     location1: "se",
     location2: "oul",
     coords: [126.978, 37.5665],
-    description: "북마크 관리 및 공유를 위한 풀스택 서비스",
-    date: "2026.03",
-    tags: ["React", "TypeScript", "Supabase"],
-    type: "Project",
-    link: "https://clippi-seven.vercel.app/",
     cardOffset: { x: -20, y: -380 },
   },
 
   {
     id: 2,
-    title: "Clippi",
+    projects: [
+      {
+        title: "Clippi",
+        date: "2026.03",
+        type: "project",
+        description: "북마크 관리 및 공유를 위한 풀스택 서비스",
+        tags: ["React", "TypeScript", "supabase"],
+        link: "https://clippi-seven.vercel.app/",
+      },
+    ],
     country: "japan",
     location1: "to",
     location2: "kyo",
     coords: [139.6917, 35.6895],
-    description: "북마크 관리 및 공유를 위한 풀스택 서비스",
-    date: "2026.03",
-    tags: ["React", "TypeScript", "Supabase"],
-    type: "Project",
-    link: "https://clippi-seven.vercel.app/",
     cardOffset: { x: 120, y: -100 },
   },
 ];
